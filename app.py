7c60791eaf410314ddbc7f469a9c93a6658cd880
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-19 23:41:01 +0900

    ハッカソン行くぞ!!

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from key_api import get_key
 from image_api import get_image
 from ginza_jp_api import ginza
 from topkey import select_topkey
+from test import great_img
 
 
 app = Flask(__name__)
@@ -25,8 +26,9 @@ def hello():
     if(hello == "EOF"):
         return render_template("mainpage.html", name="https://sports-pctr.c.yimg.jp/r/iwiz-amd/20210918-00403683-usoccer-000-1-view.jpg?cx=0&cy=0&cw=1200&ch=750")
     hello2 = ginza(hello)
-    hello3 = get_image(top_key,hello2)
-    return render_template("mainpage.html", name=hello3)
+    # hello3 = get_image(top_key,hello2)
+    hello4 = great_img(top_key,hello2)
+    return render_template("mainpage.html", name=hello4)
 
 
 
