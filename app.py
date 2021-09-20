afa6bab0784d6b3efcee1d620f03d7f256ab6d99
mercy34mercy <etukobamasatyan@gmail.com>
2021-09-20 20:43:55 +0900

    完成

--- app.py
+++ app.py
@@ -19,9 +19,15 @@ def indexs():
     return 'hello'
 
 
-@app.route('/url')
+@app.route('/url', methods=['POST'])
 def hello():
     top_key = "美女"
+    try:
+        if request.method == 'GET':
+            top_key = request.json['quali']
+            num = request.json["num"]
+    except:
+        return "EOF"
     hello = get_key(top_key)
     if(hello == "EOF"):
         return render_template("mainpage.html", name="https://sports-pctr.c.yimg.jp/r/iwiz-amd/20210918-00403683-usoccer-000-1-view.jpg?cx=0&cy=0&cw=1200&ch=750")
